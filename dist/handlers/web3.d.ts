38cec687baf56b1e54ebc0e76207b31571d0748c
VKint <dima@xp.network>
2021-08-12 20:41:29 +0530

    web3: initial nft support

--- dist/handlers/web3.d.ts
+++ dist/handlers/web3.d.ts
@@ -1,26 +1,15 @@
 import { Networkish } from "@ethersproject/networks";
-import BigNumber from "bignumber.js";
 import { Contract } from "ethers";
 import { Interface } from "ethers/lib/utils";
-import { ChainEmitter, ChainIdentifier, ChainListener, TransferEvent, UnfreezeEvent } from "../chain_handler";
-declare enum SolEventT {
-    Unfreeze = 0,
-    Transfer = 1
-}
-declare type SolEvent = {
-    readonly type: SolEventT;
-    readonly action_id: BigNumber;
-    readonly chain_nonce: BigNumber;
-    readonly to: string;
-    readonly value: BigNumber;
-};
-export declare class Web3Helper implements ChainEmitter<SolEvent, void, TransferEvent | UnfreezeEvent>, ChainListener<TransferEvent | UnfreezeEvent, string>, ChainIdentifier {
+import { ChainEmitter, ChainIdentifier, ChainListener, TransferEvent, TransferUniqueEvent, UnfreezeEvent, UnfreezeUniqueEvent } from "../chain_handler";
+declare type SupportedEvs = TransferEvent | UnfreezeEvent | TransferUniqueEvent | UnfreezeUniqueEvent;
+export declare class Web3Helper implements ChainEmitter<SupportedEvs, void, SupportedEvs>, ChainListener<SupportedEvs, string>, ChainIdentifier {
     readonly mintContract: Contract;
     readonly chainNonce: number;
     private constructor();
     static new: (provider_uri: string, pkey: string, minter: string, minterAbi: Interface, chainNonce: number, networkOpts?: Networkish | undefined) => Promise<Web3Helper>;
-    eventIter(cb: ((event: SolEvent) => Promise<void>)): Promise<void>;
-    eventHandler(ev: SolEvent): Promise<TransferEvent | UnfreezeEvent | undefined>;
-    emittedEventHandler(event: TransferEvent | UnfreezeEvent, origin_nonce: number): Promise<string>;
+    eventIter(cb: ((event: SupportedEvs) => Promise<void>)): Promise<void>;
+    eventHandler: (ev: SupportedEvs) => Promise<SupportedEvs>;
+    emittedEventHandler(event: SupportedEvs, origin_nonce: number): Promise<string>;
 }
 export {};
--- src/handlers/web3.ts
+++ src/handlers/web3.ts
@@ -2,22 +2,17 @@ import { Networkish } from "@ethersproject/networks";
 import BigNumber from "bignumber.js";
 import { Contract, providers, Wallet, BigNumber as EthBN } from "ethers";
 import { Interface } from "ethers/lib/utils";
-import { ChainEmitter, ChainIdentifier, ChainListener, TransferEvent, UnfreezeEvent } from "../chain_handler";
+import { ChainEmitter, ChainIdentifier, ChainListener, TransferEvent, TransferUniqueEvent, UnfreezeEvent, UnfreezeUniqueEvent } from "../chain_handler";
+import {NftEthNative, NftPacked} from "../encoding";
 
-enum SolEventT {
-	Unfreeze,
-	Transfer
-}
 
-type SolEvent = {
-	readonly type: SolEventT;
-	readonly action_id: BigNumber;
-	readonly chain_nonce: BigNumber,
-	readonly to: string;
-	readonly value: BigNumber;
-};
+type SupportedEvs = TransferEvent | UnfreezeEvent | TransferUniqueEvent | UnfreezeUniqueEvent;
 
-export class Web3Helper implements ChainEmitter<SolEvent, void, TransferEvent | UnfreezeEvent>, ChainListener<TransferEvent | UnfreezeEvent, string>, ChainIdentifier {
+export class Web3Helper implements
+	ChainEmitter<SupportedEvs, void, SupportedEvs>,
+	ChainListener<SupportedEvs, string>,
+	ChainIdentifier
+{
     readonly mintContract: Contract;
 	readonly chainNonce: number;
 
@@ -35,40 +30,125 @@ export class Web3Helper implements ChainEmitter<SolEvent, void, TransferEvent | 
         return new Web3Helper(mint, chainNonce);
     }
 
-	async eventIter(cb: ((event: SolEvent) => Promise<void>)): Promise<void> {
-		this.mintContract.on('Unfreeze', async (action_id: EthBN, chain_nonce: EthBN, to: string, value: BigNumber) => {
-			const ev = { type: SolEventT.Unfreeze, action_id: new BigNumber(action_id.toString()), chain_nonce: new BigNumber(chain_nonce.toString()), to, value };
+	async eventIter(cb: ((event: SupportedEvs) => Promise<void>)): Promise<void> {
+		this.mintContract.on('Unfreeze', async (action_id: EthBN, chain_nonce: EthBN, to: string, value: EthBN) => {
+			const ev = new UnfreezeEvent(
+				new BigNumber(action_id.toString()),
+				chain_nonce.toNumber(),
+				to,
+				new BigNumber(value.toString())
+			);
 			await cb(ev);
 		});
 		this.mintContract.on('Transfer', async (action_id: EthBN, chain_nonce: EthBN, to: string, value: BigNumber) => {
-			const ev = { type: SolEventT.Transfer, action_id: new BigNumber(action_id.toString()), chain_nonce: new BigNumber(chain_nonce.toString()), to, value };
+			const ev = new TransferEvent(
+				new BigNumber(action_id.toString()),
+				chain_nonce.toNumber(),
+				to,
+				new BigNumber(value.toString())
+			)
 			await cb(ev);
 		});
+		this.mintContract.on('UnfreezeNft', async (action_id: EthBN, to: string, data: string) => {
+			const prot = NftPacked.deserializeBinary(
+				Buffer.from(data, 'base64')
+			);
+
+			const ev = new UnfreezeUniqueEvent(
+				new BigNumber(action_id.toString()),
+				prot.getChainNonce(),
+				to,
+				prot.getData_asU8()
+			);
+
+			await cb(ev);
+		});
+		this.mintContract.on('TransferErc721', async (action_id: EthBN, chain_nonce: EthBN, to: string, id: BigNumber, contract_addr: string) => {
+			const prot = new NftEthNative();
+			prot.setId(id.toString());
+			prot.setNftKind(NftEthNative.NftKind.ERC721);
+			prot.setContractAddr(contract_addr);
+
+			const ev = new TransferUniqueEvent(
+				new BigNumber(action_id.toString()),
+				chain_nonce.toNumber(),
+				to,
+				prot.serializeBinary()
+			)
+
+			await cb(ev);
+		});
+		this.mintContract.on('TransferErc1155', async (action_id: EthBN, chain_nonce: EthBN, to: string, id: BigNumber, contract_addr: string) => {
+			const prot = new NftEthNative();
+			prot.setId(id.toString());
+			prot.setNftKind(NftEthNative.NftKind.ERC1155);
+			prot.setContractAddr(contract_addr);
+
+			const ev = new TransferUniqueEvent(
+				new BigNumber(action_id.toString()),
+				chain_nonce.toNumber(),
+				to,
+				prot.serializeBinary()
+			)
+
+			await cb(ev);
+		})
 	}
 
-	async eventHandler(ev: SolEvent): Promise<TransferEvent | UnfreezeEvent | undefined> {
-		switch (ev.type) {
-			case SolEventT.Unfreeze:
-				return new UnfreezeEvent(ev.action_id, ev.chain_nonce.toNumber(), ev.to, ev.value);
-			case SolEventT.Transfer:
-				return new TransferEvent(ev.action_id, ev.chain_nonce.toNumber(), ev.to, ev.value);
-		}
-	}
+	public eventHandler = async (ev: SupportedEvs) => ev;
 
-    async emittedEventHandler(event: TransferEvent | UnfreezeEvent, origin_nonce: number): Promise<string> {
+    async emittedEventHandler(event: SupportedEvs, origin_nonce: number): Promise<string> {
 		let kind: string;
 		let action: string;
 		let tx: providers.TransactionResponse;
 		if (event instanceof TransferEvent) {
 			action = event.action_id.toString();
-			console.log(`target: ${event.to}, value: ${event.value}`)
             tx = await this.mintContract.validate_transfer(action, origin_nonce, event.to, event.value.toString());
 			kind = "transfer"
         } else if (event instanceof UnfreezeEvent) {
 			action = event.id.toString();
             tx = await this.mintContract.validate_unfreeze(action, event.to, event.value.toString());
 			kind = "unfreeze"
-        } else {
+        } else if (event instanceof TransferUniqueEvent) {
+			action = event.action_id.toString();
+			const encoded = new NftPacked();
+			encoded.setChainNonce(origin_nonce);
+			encoded.setData(event.id);
+
+			const buf = Buffer.from(encoded.serializeBinary())
+
+			tx = await this.mintContract.validate_transfer_nft(
+				action,
+				event.to,
+				buf.toString('base64')
+			);
+			kind = "transfer_nft"
+		} else if (event instanceof UnfreezeUniqueEvent) {
+			action = event.id.toString();
+			const encoded = NftEthNative.deserializeBinary(event.nft_id);
+			
+			switch (encoded.getNftKind()) {
+				case NftEthNative.NftKind.ERC1155: {
+					tx = await this.mintContract.validate_unfreeze_erc1155(
+						action,
+						event.to,
+						new BigNumber(encoded.getId()),
+						encoded.getContractAddr()
+					)
+					break;
+				}
+				case NftEthNative.NftKind.ERC721: {
+					tx = await this.mintContract.validate_unfreeze_erc721(
+						action,
+						event.to,
+						new BigNumber(encoded.getId()),
+						encoded.getContractAddr()
+					)
+					break;
+				}
+			}
+			kind = "unfreeze_nft"
+		} else {
             throw Error("Unsupported event!");
         }
 	
