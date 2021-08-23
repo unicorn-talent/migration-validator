667f99ca00ca78e0e7b4a0107bf956f535a4a835
rupansh <rupanshsekar@hotmail.com>
2021-08-23 14:10:58 +0530

    update config for testing

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,10 @@ const config = {
     elrond_node: 'https://devnet-api.elrond.com',
     // Private Key File
     private_key:
-        '../XP.network-Elrond-Migration/erd1yflgh7duhhvpkqkqqjrcnz7j6pqnhy8kepglkk6k8h8dfu3as3ysdcxan8.pem',
+        './validator2.pem',
     // Elrond minter contract address
     elrond_minter:
-        'erd1qqqqqqqqqqqqqpgqe8lzlc2husrrthhyavhcj50kpneqzf9ms3ys4anmfd',
+        'erd1qqqqqqqqqqqqqpgquvs7p7lj2th4rge03rfpa8trlthwekd4k4as966la8',
     // Workaround Elrond Event websocket
     elrond_ev_socket: 'ws://localhost:3000',
 	web3: [
@@ -20,9 +20,9 @@ const config = {
 			nonce: 0x3
 		},
 		{
-			node: "https://data-seed-prebsc-2-s1.binance.org:8545",
-			pkey: "0xb43ddd27cfb99201cb94c11cf5982c9c04582dfd2b64f656cc01bbf96eec0cf6",
-			minter: "0x158D8366a2dfFEdCaC1e1B3ACDcC59a9941dd625",
+			node: "https://data-seed-prebsc-1-s2.binance.org:8545",
+			pkey: "0x5dca57fe4b6ed2572052efa01b37cc7e20ec1eee3dc3088ca0f5ebb59f875756",
+			minter: "0x1D6D4c33b58317Ca09A900737b6c24D2e1d1aBe6",
 			nonce: 0x4
 		},
 		{
